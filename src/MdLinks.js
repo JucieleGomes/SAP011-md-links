ddccddd138fdc27df5be5972c6f1d2ff1212cd7d
JucieleGomes <jucielegomes.s@hotmail.com>
2023-10-25 22:15:36 -0300

    feat atualizações

--- src/MdLinks.js
+++ src/MdLinks.js
@@ -1,47 +1,46 @@
-
 const fs = require("fs");
 const path = require("path");
 
-function readFile(filePath){
- return new Promise((resolve, reject) => {
-    fs.readFile(filePath,"utf8", (err, data) => {
+
+function readFile(filePath) {
+  return new Promise((resolve, reject) => {
+    fs.readFile(filePath, "utf8", (err, data) => {
       if (err) reject(err);
       resolve(data);
-  })
-});
-
+    });
+  });
 }
+
 const root = './test/Files';
-let data = {};
+let data = [];
 let counter = 0;
 
 function mdLinks(dir) {
-  const directories = fs.readdirSync(dir); 
+  const directories = fs.readdirSync(dir);
 
-  for(directory of directories){
-    const dirPath = path.join(dir, directory)
-    const stats = fs.statSync(dirPath)
-  
-    if(stats.isDirectory()){
-      mdLinks(dirPath)
-    }else{
-      data[counter] = {  
-        Name: path.dirname(dirPath),
+  for (const directory of directories) {
+    const dirPath = path.join(dir, directory);
+    const stats = fs.statSync(dirPath);
+
+    if (stats.isDirectory()) {
+      mdLinks(dirPath);
+
+    } else if (path.extname(dirPath) === '.md') {
+      data.push({
+        Name: path.basename(dirPath),
         Path: dirPath,
-        type: path.extname(dirPath)
+        type: path.extname(dirPath),
+      });
+
+      counter += 1;
     }
-
-    counter =+ 1;
-
   }
-
-}
 }
 
 mdLinks(root);
 
 console.log(data);
 
+module.exports = { readFile, mdLinks };
 
 
-module.exports = {readFile, mdLinks}
--- src/cli.js
+++ src/cli.js
@@ -1,44 +1,48 @@
 #!/usr/bin/env node
 
-const { readFile, mdLinks } = require('./MdLinks.js');
+const { readFile } = require('./MdLinks.js');
 
 const input = process.argv;
 const filePath = input[2];
 
-   readFile(filePath, 'utf-8')
-  .then((fileContent) => {
-    const regex = /https?:\/\/[^\s/$.?#].[^\s]*/g;
-    const links = fileContent.match(regex);
 
-    if (links) {
-      for (let index = 0; index < links.length; index++) {
-        const link = links[index];
+  readFile(filePath, 'utf-8')
+    .then((fileContent) => {
+  
+        const regex = /https?:\/\/[^\s/$.?#].[^\s]*/g;
+        const links = fileContent.match(regex);
+    
+        if (links) {
+          for (let index = 0; index < links.length; index++) {
+            const link = links[index];
+            validateLinks(link); 
+          }
+        } else {
+          console.log('Nenhum link encontrado no arquivo.');
+        }
+    }) 
+    .catch((error) => {
+      console.error('Ocorreu um erro ao ler o arquivo:', error);
+    });
 
-   fetch(link) 
-  .then(response => {
-    const url = response.url;
-    const status = response.status;
 
-    if (!response.ok) {
-     console.log(`Invalid URL: ${url} Status: ${status}`);
-    }
-    if (response.status === 200) {
-      console.error(`Valid URL: ${url} Status: ${status}`);
-    }
-  })
-  .catch(error => {
-    console.error('Ocorreu um erro na requisição', error);
-  });
+function validateLinks(link) {
+  fetch(link)
+    .then((response) => {
+      const url = response.url;
+      const status = response.status;
+
+      if (!response.ok) {
+        console.log(`Invalid URL: ${url} Status: ${status}`);
+      } else if (response.status === 200) {
+        console.log(`Valid URL: ${url} Status: ${status}`);
       }
-    } else {
-      console.log('Nenhum link encontrado no arquivo.');
-    }
-  })
-  .catch((error) => {
-    console.error('Ocorreu um erro ao ler o arquivo:', error);
-  });
+    })
+    .catch((error) => {
+      console.error('Ocorreu um erro na requisição', error);
+    });
+}
 
 
 
 
- 
